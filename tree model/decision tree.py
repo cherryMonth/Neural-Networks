7390a21ba4473c33da2fff6399a1d3f920170aaf
cherrymonth <1115064450@qq.com>
2018-12-12 19:12:18 +0800

    添加决策树

--- tree model/decision tree.py
+++ tree model/decision tree.py
@@ -37,7 +37,7 @@ classifier.fit(X_train, y_train)
 score = classifier.score(X_test, y_test)
 print(score)
 y_pred = classifier.predict(X_test)
-
+print(y_pred.shape)
 from sklearn.metrics import classification_report
 
 print(classification_report(y_true=y_test, y_pred=y_pred))
